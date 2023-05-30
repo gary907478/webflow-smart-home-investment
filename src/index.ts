1ce2d65634a63e56f0f2a78f59ca218fc8a398b5
Gary Zhang <gary@moocads.com>
2023-05-30 13:57:00 -0400

    test publish to production

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
+  // Get the current URL path
+  const currentPath = window.location.pathname;
+
+  // Check if the current path starts with "/blogs/"
+  if (currentPath.startsWith('/services/')) {
+    // Find the navbar link that corresponds to the current page
+    const navbarLinks = document.querySelector('#nav-services');
+    navbarLinks.classList.add('nav-current-active');
+  }
+  // Check if the current path starts with "/blogs/"
+  if (currentPath.startsWith('/blog/')) {
+    // Find the navbar link that corresponds to the current page
+    const navbarLinks = document.querySelector('#nav-blogs');
+    navbarLinks.classList.add('nav-current-active');
+  }
   const name = 'John Doe';
   greetUser(name);
 });
