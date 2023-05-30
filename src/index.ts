d5f4bc9df3339d98ac3db61c4d0d0485566d6dd1
Gary Zhang <gary@moocads.com>
2023-05-30 14:42:17 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ window.Webflow.push(() => {
   // Check if the current path starts with "/blogs/"
   if (currentPath.startsWith('/blog/')) {
     // Find the navbar link that corresponds to the current page
-    const navbarLinks = document.querySelector('#nav-blogs');
-    navbarLinks.classList.add('nav-current-active');
+    // const navbarLinks = document.querySelector('#nav-blogs');
+    // navbarLinks.classList.add('nav-current-active');
   }
   const name = 'Test Name';
   greetUser(name);
